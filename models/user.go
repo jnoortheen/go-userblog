08b677b1f2077a297e439e87b9d6b9d7bb69fef6
jnoortheen <jnoortheen@gmail.com>
2017-04-13 01:56:12 +0530

    feat: created user model
    with command `soda g model -d user name pwd email salt:uuid`

---
+++ models/user.go
@@ -0,0 +1,57 @@
+package models
+
+import (
+	"encoding/json"
+	"github.com/markbates/pop"
+	"github.com/markbates/validate"
+	"github.com/markbates/validate/validators"
+	"github.com/satori/go.uuid"
+	"time"
+)
+
+type User struct {
+	ID        uuid.UUID `json:"id" db:"id"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+	Name      string    `json:"name" db:"name"`
+	Pwd       string    `json:"pwd" db:"pwd"`
+	Email     string    `json:"email" db:"email"`
+	Salt      uuid.UUID `json:"salt" db:"salt"`
+}
+
+// String is not required by pop and may be deleted
+func (u User) String() string {
+	ju, _ := json.Marshal(u)
+	return string(ju)
+}
+
+// Users is not required by pop and may be deleted
+type Users []User
+
+// String is not required by pop and may be deleted
+func (u Users) String() string {
+	ju, _ := json.Marshal(u)
+	return string(ju)
+}
+
+// Validate gets run everytime you call a "pop.Validate" method.
+// This method is not required and may be deleted.
+func (u *User) Validate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.Validate(
+		&validators.StringIsPresent{Field: u.Name, Name: "Name"},
+		&validators.StringIsPresent{Field: u.Pwd, Name: "Pwd"},
+		&validators.StringIsPresent{Field: u.Email, Name: "Email"},
+	), nil
+}
+
+// ValidateSave gets run everytime you call "pop.ValidateSave" method.
+// This method is not required and may be deleted.
+func (u *User) ValidateSave(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateUpdate gets run everytime you call "pop.ValidateUpdate" method.
+// This method is not required and may be deleted.
+func (u *User) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
