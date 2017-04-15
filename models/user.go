80aacf63241ed4d2a2b67b0a2678f309d78b4fad
jnoortheen <jnoortheen@gmail.com>
2017-04-15 23:39:41 +0530

    feat: create a new user function to create salted password

--- models/user.go
+++ models/user.go
@@ -3,6 +3,7 @@ package models
 import (
 	"encoding/json"
 	"github.com/markbates/pop"
+	"github.com/markbates/pop/nulls"
 	"github.com/markbates/validate"
 	"github.com/markbates/validate/validators"
 	"github.com/satori/go.uuid"
@@ -10,13 +11,13 @@ import (
 )
 
 type User struct {
-	ID        uuid.UUID `json:"id" db:"id"`
-	CreatedAt time.Time `json:"created_at" db:"created_at"`
-	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
-	Name      string    `json:"name" db:"name"`
-	Pwd       string    `json:"pwd" db:"pwd"`
-	Email     string    `json:"email" db:"email"`
-	Salt      uuid.UUID `json:"salt" db:"salt"`
+	ID        uuid.UUID    `json:"id" db:"id"`
+	CreatedAt time.Time    `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time    `json:"updated_at" db:"updated_at"`
+	Name      string       `json:"name" db:"name"`
+	Pwd       string       `json:"pwd" db:"pwd"`
+	Email     nulls.String `json:"email" db:"email"`
+	Salt      uuid.UUID    `json:"salt" db:"salt"`
 }
 
 // String is not required by pop and may be deleted
@@ -40,7 +41,6 @@ func (u *User) Validate(tx *pop.Connection) (*validate.Errors, error) {
 	return validate.Validate(
 		&validators.StringIsPresent{Field: u.Name, Name: "Name"},
 		&validators.StringIsPresent{Field: u.Pwd, Name: "Pwd"},
-		&validators.StringIsPresent{Field: u.Email, Name: "Email"},
 	), nil
 }
 
@@ -54,4 +54,11 @@ func (u *User) ValidateSave(tx *pop.Connection) (*validate.Errors, error) {
 // This method is not required and may be deleted.
 func (u *User) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
 	return validate.NewErrors(), nil
+}
+
+// get a new user struct that can directly be used to create user record using pop
+func NewUser(name string, plainPwd string, email string) *User {
+	salt := uuid.NewV1()
+	pwd := uuid.NewV3(salt, plainPwd).String()
+	return &User{Name: name, Email: nulls.String{String:email, Valid:true}, Pwd: pwd, Salt: salt}
 }
--- models/user_test.go
+++ models/user_test.go
@@ -1,7 +1,28 @@
 package models_test
 
-import "testing"
+import (
+	"muserblog/models"
+)
 
-func Test_User(t *testing.T) {
-	t.Fatal("This test needs to be implemented!")
+var (
+	userName     = "test_user"
+	userPwdPlain = "password"
+	userEmail    = "user@mail.com"
+)
+
+func userForTest() *models.User {
+	return models.NewUser(userName, userPwdPlain, userEmail)
+}
+
+func countUsers(as *ModelSuite) int {
+	usersCount, err := as.DB.Count(models.Users{})
+	as.NoError(err)
+	return usersCount
+}
+
+func (as *ModelSuite) Test_User() {
+	prevCount := countUsers(as)
+	user := userForTest()
+	as.NoError(as.DB.Create(user))
+	as.Equal(countUsers(as)-prevCount, 1)
 }
