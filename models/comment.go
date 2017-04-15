bfea05f7ee21dd0d72a00f5511d79004153038ad
jnoortheen <jnoortheen@gmail.com>
2017-04-16 00:37:14 +0530

    feat: generating comment model with generator
    `buffalo db g m comment content:text post_id:uuid author_id:uuid`

---
+++ models/comment.go
@@ -0,0 +1,54 @@
+package models
+
+import (
+	"encoding/json"
+	"github.com/markbates/pop"
+	"github.com/markbates/validate"
+	"github.com/markbates/validate/validators"
+	"github.com/satori/go.uuid"
+	"time"
+)
+
+type Comment struct {
+	ID        uuid.UUID `json:"id" db:"id"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+	Content   string    `json:"content" db:"content"`
+	PostID    uuid.UUID `json:"post_id" db:"post_id"`
+	UserID    uuid.UUID `json:"user_id" db:"user_id"`
+}
+
+// String is not required by pop and may be deleted
+func (c Comment) String() string {
+	jc, _ := json.Marshal(c)
+	return string(jc)
+}
+
+// Comments is not required by pop and may be deleted
+type Comments []Comment
+
+// String is not required by pop and may be deleted
+func (c Comments) String() string {
+	jc, _ := json.Marshal(c)
+	return string(jc)
+}
+
+// Validate gets run everytime you call a "pop.Validate" method.
+// This method is not required and may be deleted.
+func (c *Comment) Validate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.Validate(
+		&validators.StringIsPresent{Field: c.Content, Name: "Content"},
+	), nil
+}
+
+// ValidateSave gets run everytime you call "pop.ValidateSave" method.
+// This method is not required and may be deleted.
+func (c *Comment) ValidateSave(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateUpdate gets run everytime you call "pop.ValidateUpdate" method.
+// This method is not required and may be deleted.
+func (c *Comment) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
