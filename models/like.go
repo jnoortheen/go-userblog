58d2f9e029b612b61d24eb9c0005763d9df7e32a
jnoortheen <jnoortheen@gmail.com>
2017-04-16 01:01:18 +0530

    feat: generated a table to keep likes count
    using this command
    `buffalo db g m like post_id:uuid user_id:uuid`

---
+++ models/like.go
@@ -0,0 +1,50 @@
+package models
+
+import (
+	"encoding/json"
+	"github.com/markbates/pop"
+	"github.com/markbates/validate"
+	"github.com/satori/go.uuid"
+	"time"
+)
+
+type Like struct {
+	ID        uuid.UUID `json:"id" db:"id"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+	PostID    uuid.UUID `json:"post_id" db:"post_id"`
+	UserID    uuid.UUID `json:"user_id" db:"user_id"`
+}
+
+// String is not required by pop and may be deleted
+func (l Like) String() string {
+	jl, _ := json.Marshal(l)
+	return string(jl)
+}
+
+// Likes is not required by pop and may be deleted
+type Likes []Like
+
+// String is not required by pop and may be deleted
+func (l Likes) String() string {
+	jl, _ := json.Marshal(l)
+	return string(jl)
+}
+
+// Validate gets run everytime you call a "pop.Validate" method.
+// This method is not required and may be deleted.
+func (l *Like) Validate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateSave gets run everytime you call "pop.ValidateSave" method.
+// This method is not required and may be deleted.
+func (l *Like) ValidateSave(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateUpdate gets run everytime you call "pop.ValidateUpdate" method.
+// This method is not required and may be deleted.
+func (l *Like) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
