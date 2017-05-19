e9b2b33dfdaad7ceb3894c609c99f54556ac80be
jnoortheen <jnoortheen@gmail.com>
2017-05-19 22:20:42 +0530

    feat: adding jquery-comments to project

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,6 +1,3 @@
-require('expose-loader?$!expose-loader?jQuery!jquery');
+require("expose-loader?$!expose-loader?jQuery!jquery");
 require("bootstrap/dist/js/bootstrap.js");
-
-$(() => {
-
-});
+require("jquery-comments/js/jquery-comments.js");
