fbfadf2976e397b018afa60582dc1511ea1a83d3
jnoortheen <jnoortheen@gmail.com>
2017-05-20 13:19:36 +0530

    test: adding for comment#update

--- actions/comments_test.go
+++ actions/comments_test.go
@@ -3,15 +3,19 @@ package actions_test
 import (
 	"muserblog/models"
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
-func commentForTest(user *models.User, post *models.Post) *models.Comment {
-	return &models.Comment{Content: postContent, UserID: user.ID, PostID: post.ID}
-}
-
 func (as *ActionSuite) countComments() int {
 	return as.Count(models.Comment{})
+}
+
+func (as *ActionSuite) commentPath(post *models.Post) string {
+	return fmt.Sprintf("/%s/comments", post.ID.String())
+}
+func (as *ActionSuite) commentUpdatePath(post *models.Post, comment *models.Comment) string {
+	return fmt.Sprintf("/%s/comments/%s", post.ID.String(), comment.ID.String())
 }
 
 func (as *ActionSuite) Test_CommentsResource_List() {
@@ -20,7 +24,7 @@ func (as *ActionSuite) Test_CommentsResource_List() {
 	for _, content := range contents {
 		as.DB.Create(&models.Comment{Content: content, UserID: user.ID, PostID: post.ID})
 	}
-	resp := as.JSON("/comments").Get()
+	resp := as.JSON(as.commentPath(post)).Get()
 	comments := &models.Comments{}
 
 	if err := json.NewDecoder(resp.Body).Decode(comments); err != nil {
@@ -31,33 +35,57 @@ func (as *ActionSuite) Test_CommentsResource_List() {
 
 func (as *ActionSuite) Test_CommentsResource_Create() {
 	// initial variables
-	user, post := createPost(as)
-	comment := commentForTest(user, post)
 	prevCount := as.countComments()
+	user, post := createPost(as)
+	content := "content"
+	comment := &models.Comment{Content: content}
 
 	// without signin
-	resp := as.JSON("/comments").Post(comment)
+	resp := as.JSON(as.commentPath(post)).Post(comment)
 	as.Equal(http.StatusUnauthorized, resp.Code)
 
 	signinUser(as, user)
 
 	// create post
-	resp = as.JSON("/comments").Post(comment)
+	resp = as.JSON(as.commentPath(post)).Post(comment)
 	as.Equal(201, resp.Code)
-	as.Equal(1, as.countPosts()-prevCount)
+	as.Equal(1, as.countComments()-prevCount)
 
 	// check post exists in db
 	as.DB.First(comment)
-	user.WithName(as.DB)
-	as.Equal(postContent, comment.Content)
+	as.Equal(content, comment.Content)
 	as.Equal(comment.UserID, user.ID)
 	as.Equal(comment.PostID, post.ID)
 }
 
 func (as *ActionSuite) Test_CommentsResource_Update() {
-	as.Fail("Not Implemented!")
+	// initial variables
+	user, post := createPost(as)
+	content := "content"
+	comment := &models.Comment{Content: content, UserID: user.ID, PostID: post.ID}
+	as.DB.Create(comment)
+	prevCount := as.countComments()
+
+	// without signin
+	resp := as.JSON(as.commentUpdatePath(post, comment)).Put(comment)
+	as.Equal(http.StatusUnauthorized, resp.Code)
+
+	//after signin
+	signinUser(as, user)
+
+	// update post
+	comment.Content = "updated content"
+	resp = as.JSON(as.commentUpdatePath(post, comment)).Put(comment)
+	as.Equal(200, resp.Code)
+	as.Equal(0, as.countComments()-prevCount)
+
+	// get comment from db
+	as.DB.Find(comment, comment.ID)
+	as.Equal("updated content", comment.Content)
+	as.Equal(comment.UserID, user.ID)
+	as.Equal(comment.PostID, post.ID)
 }
 
-//func (as *ActionSuite) Test_CommentsResource_Destroy() {
-//	as.Fail("Not Implemented!")
-//}
+func (as *ActionSuite) Test_CommentsResource_Destroy() {
+	as.Fail("Not Implemented!")
+}
